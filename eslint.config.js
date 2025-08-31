0452a0a50b6e359679ead939b5590b55af4a556d
kazaminn <90266173+kazaminn@users.noreply.github.com>
2025-08-31 23:40:04 +0900

    🔧 chore: add storybook, and fix errors
    - Storybook対応
    - ESLintインポートエラー修正
    - envファイル読み込み失敗でundefinedになるエラー修正
    - commitlint追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook';
-
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier/flat';
-import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -20,7 +18,6 @@ export default tseslint.config(
     },
 
     plugins: {
-      import: importPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -30,24 +27,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      'import/order': {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-          'object',
-          'type',
-        ],
-        'newlines-between': 'always',
-        pathGroupsExcludedImportTypes: ['builtin'],
-        alphabetize: { order: 'asc', caseInsensitive: true },
-        pathGroups: [
-          { pattern: 'src/types/**', group: 'internal', position: 'before' },
-        ],
-      },
     },
   },
   storybook.configs['flat/recommended'],
